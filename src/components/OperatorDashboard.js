090277112f21ea0963131d94ea33ac2689cafa24
valtrilabs <valtrilabs@gmail.com>
2025-04-22 15:47:49 +0530

    moved analytics from operator to payment2

--- src/components/OperatorDashboard.js
+++ src/components/OperatorDashboard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { FaPlus, FaEdit, FaTrash, FaCheck, FaTimes, FaUtensils, FaList, FaClock, FaHamburger, FaPrint } from 'react-icons/fa';
+import { FaPlus, FaEdit, FaTrash, FaCheck, FaTimes, FaUtensils, FaList, FaClock, FaHamburger, FaPrint, FaRupeeSign } from 'react-icons/fa';
 
 function OperatorDashboard() {
   const [orders, setOrders] = useState([]);
