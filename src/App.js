c7b5d75d6a9140e35635a442fda40178587c75a2
valtrilabs <valtrilabs@gmail.com>
2025-05-03 02:53:23 +0530

    manul order by waiter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Menu from './components/Menu';
+// import OperatorDashboard from './components/OperatorDashboard';
+// import PaymentDashboard from './components/PaymentDashboard';
+// import QRCodeGenerator from './components/QRCodeGenerator';
+// import ScanQR from './components/ScanQR';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/order" element={<Menu />} />
+//         <Route path="/operator" element={<OperatorDashboard />} />
+//         <Route path="/payment" element={<PaymentDashboard />} />
+//         <Route path="/qr" element={<QRCodeGenerator />} />
+//         <Route path="/scan-qr" element={<ScanQR />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScanQR from './components/ScanQR';
 import Menu from './components/Menu';
 import OperatorDashboard from './components/OperatorDashboard';
 import PaymentDashboard from './components/PaymentDashboard';
-import QRCodeGenerator from './components/QRCodeGenerator';
-import ScanQR from './components/ScanQR';
+import WaiterOrder from './components/WaiterOrder';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/scan-qr" element={<ScanQR />} />
         <Route path="/order" element={<Menu />} />
         <Route path="/operator" element={<OperatorDashboard />} />
         <Route path="/payment" element={<PaymentDashboard />} />
-        <Route path="/qr" element={<QRCodeGenerator />} />
-        <Route path="/scan-qr" element={<ScanQR />} />
+        <Route path="/waiter" element={<WaiterOrder />} />
+        <Route path="/" element={<ScanQR />} />
       </Routes>
     </Router>
   );
