0adc3d8bf2dbf18b17a21b883394972912aa3879
valtrilabs <valtrilabs@gmail.com>
2025-05-03 19:59:39 +0530

    KOT

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Menu from './components/Menu';
-// import OperatorDashboard from './components/OperatorDashboard';
-// import PaymentDashboard from './components/PaymentDashboard';
-// import QRCodeGenerator from './components/QRCodeGenerator';
-// import ScanQR from './components/ScanQR';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/order" element={<Menu />} />
-//         <Route path="/operator" element={<OperatorDashboard />} />
-//         <Route path="/payment" element={<PaymentDashboard />} />
-//         <Route path="/qr" element={<QRCodeGenerator />} />
-//         <Route path="/scan-qr" element={<ScanQR />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ScanQR from './components/ScanQR';
@@ -28,6 +5,7 @@ import Menu from './components/Menu';
 import OperatorDashboard from './components/OperatorDashboard';
 import PaymentDashboard from './components/PaymentDashboard';
 import WaiterOrder from './components/WaiterOrder';
+import QRCodeGenerator from './components/QRCodeGenerator'; // Added missing import
 
 function App() {
   return (
